9f2a66c2c902bf2a01b7b90ffb9419f736433024
tumofreduclips <40761105+tumofreduclips@users.noreply.github.com>
2019-07-12 19:04:00 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+var firstNumber = document.getElementById('firstNumber').value;
+var secondNumber = document.getElementById('secondNumber').value;
+var operationSign = document.getElementById('operationSign').value;
+
+
+
+
+console.log(firstNumber);
+console.log(secondNumber);
+console.log(operationSign);
